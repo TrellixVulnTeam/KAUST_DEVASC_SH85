4546fd385a03fad5afba8f0cf4b7e7639aadf865
gsemrl <gsemrl@nil.com>
2021-12-01 06:37:40 +0100

    Parse library

--- iosxr/parse-iosxr.py
+++ iosxr/parse-iosxr.py
@@ -2,13 +2,23 @@ import argparse
 import json
 import yaml
 
+
 def processFile(jsonFile):
+    device = 'ASR-1'
+    segment = 'Controller'
+    contents = ''
+    #parse JSON from device
     with open(jsonFile,'r') as json_file:
         jsonObj = json.load(json_file)
         for c in jsonObj["Response"]["Get"]["Operational"]["Controllers"]["ControllerDescriptionTable"]["Controller"]:
             controllerName = c['Controller']
             state = c['State']
-            print(f'{controllerName}\t{state}')
+            contents += f'{device}\t{segment}\t{controllerName}\t{state}\n'
+    #save the file
+    with open("parsedContent.txt", "w") as f:
+        f.write(contents)
+
+
 
         
 
